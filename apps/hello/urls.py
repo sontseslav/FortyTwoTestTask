65a1087d781c0dd00b99adc96f82a0ae8c42cd09
sontseslav <s.khvalinsky@gmail.com>
2017-02-15 00:37:55 +0200

    ticket 1: add static support to urls

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
 )
+urlpatterns += staticfiles_urlpatterns()
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -7,3 +9,4 @@ urlpatterns = patterns(
     url(r'^', include('apps.hello.urls')),
     url(r'^admin/', include(admin.site.urls), name="admin"),
 )
+urlpatterns += staticfiles_urlpatterns()
