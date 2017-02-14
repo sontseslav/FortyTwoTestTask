cd5dd64e8d8586ca47edefb9f9088ac953305017
Stanislav <sontseslav@users.noreply.github.com>
2017-02-15 00:28:01 +0200

    ticket 1: fix settings module

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -105,7 +105,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
