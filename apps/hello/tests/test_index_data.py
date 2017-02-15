e10abf084a400afec2e41e9b196cc0719414c8ca
sontseslav <s.khvalinsky@gmail.com>
2017-02-15 22:50:51 +0200

    ticket: 1 add view to show hardcoded data

--- apps/hello/tests/test_index_data.py
+++ apps/hello/tests/test_index_data.py
@@ -20,10 +20,10 @@ class IndexDataTests(TestCase):
         self.assertTrue('42 Coffee Cups Test Assignment' in resp.content)
         self.assertTrue('Stanislav' in resp.content)
         self.assertTrue('Khvalinsky' in resp.content)
-        self.assertTrue('s[dot]khvalinsky[at]gmail[dot]com' in resp.content)
+        self.assertTrue('s.khvalinsky@gmail.com' in resp.content)
         self.assertTrue(
-            'Jabber: Stanislav_Khvalinsky[at]42cc[dot]co' in resp.content
+            'stanislav_khvalinsky@42cc.co' in resp.content
         )
         self.assertTrue('28.09.1984' in resp.content)
-        self.assertTrue('still[dot]hope' in resp.content)
+        self.assertTrue('still.hope' in resp.content)
         self.assertEqual(2, resp.content.count('Don\'t be disclosed'))
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -2,4 +2,11 @@ from django.views.generic.base import TemplateView
 
 
 class IndexView(TemplateView):
-    pass
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        return super(IndexView, self).get(request, *args, **kwargs)
+
+    def context(self, **kwargs):
+        context = super(IndexView, self).context(**kwargs)
+        return context
