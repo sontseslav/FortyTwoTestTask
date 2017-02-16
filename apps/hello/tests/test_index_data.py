ddc55c03642380c1740779e62a6eeb10cc64c322
sontseslav <s.khvalinsky@gmail.com>
2017-02-16 23:40:26 +0200

    ticket:1 add test for model Person

--- apps/hello/tests/test_index_data.py
+++ apps/hello/tests/test_index_data.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from apps.hello.models import Person
 
 
 class IndexDataTests(TestCase):
+    fixtures = ['hello.json']
 
     def test_index_reachable(self):
         "is index page reachable by url name"
@@ -17,6 +19,7 @@ class IndexDataTests(TestCase):
     def test_index_hardcoded_data(self):
         "is view returning hardcoded data"
         resp = self.client.get(reverse('index'))
+        print resp
         self.assertTrue('42 Coffee Cups Test Assignment' in resp.content)
         self.assertTrue('Stanislav' in resp.content)
         self.assertTrue('Khvalinsky' in resp.content)
@@ -27,3 +30,22 @@ class IndexDataTests(TestCase):
         self.assertTrue('28.09.1984' in resp.content)
         self.assertTrue('still.hope' in resp.content)
         self.assertEqual(2, resp.content.count('Don\'t be disclosed'))
+
+    def test_person_model(self):
+        resp = self.client.get(reverse('index'))
+        try:
+            person = Person.objects.get(pk=1)
+        except Person.DoesNotExist:
+            raise AssertionError("Person entity with id 1 does not exisits")
+        self.assertContains(resp, person.name)
+        self.assertContains(resp, person.surname)
+        # fix data representation: yyyy-mm-dd => dd.mm.yyyy
+        date = '.'.join(date_of_birth.split('-')[:-1])
+        print date
+        self.assertContains(resp, person.date)
+        self.assertContains(resp, person.bio)
+        self.assertContains(resp, person.email)
+        self.assertContains(resp, person.jabber)
+        self.assertContains(resp, person.skype)
+        self.assertContains(resp, person.other_contacts)
+        self.assertContains(resp, person.title)
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,12 +1,22 @@
 from django.views.generic.base import TemplateView
+from django.http import Http404
+from apps.hello.models import Person
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    model = Person
+    content = None
 
     def get(self, request, *args, **kwargs):
+        try:
+            self.content = Person.objects.get(pk=1)
+        except KeyError:
+            # use template
+            raise Http404
         return super(IndexView, self).get(request, *args, **kwargs)
 
     def context(self, **kwargs):
         context = super(IndexView, self).context(**kwargs)
+        content['person'] = self.content
         return context
