70758ae0441c0c4f8222eff23a974eea682576d5
sontseslav <s.khvalinsky@gmail.com>
2017-02-20 22:13:21 +0200

    ticket:3_add test for middleware

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -17,15 +17,4 @@ class Person(models.Model):
 
 
 class HttpRequest(models.Model):
-    # max - CONNECT
-    method = models.CharField(max_length=7)
-    path = models.CharField(max_length=255)
-    server_protocol = models.CharField(max_length=12)
-    status = models.PositiveSmallIntegerField()
-    response_length = models.PositiveIntegerField()
-    date = models.DateTimeField(auto_now=True, auto_now_add=True)
-    class Meta:
-        ordering = ['date']
-
-    def __unicode__(self):
-        return self.method + " " + self.path
+    pass
