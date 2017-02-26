ca4a0dc17eaa22788519722a952d2735431bf0c0
sontseslav <s.khvalinsky@gmail.com>
2017-02-26 17:48:33 +0200

    ticket:1 add test for the model

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -2,15 +2,4 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=25)
-    surname = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
-    bio = models.TextField()
-    email = models.EmailField(max_length=50)
-    jabber = models.CharField(max_length=50)
-    skype = models.CharField(max_length=50)
-    other_contacts = models.CharField(max_length=200)
-    title = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return unicode(self.name + " " + self.surname)
+    pass
--- apps/hello/tests/test_index_data.py
+++ apps/hello/tests/test_index_data.py
@@ -29,3 +29,23 @@ class IndexDataTests(TestCase):
         self.assertTrue('28.09.1984' in resp.content)
         self.assertTrue('still.hope' in resp.content)
         self.assertEqual(2, resp.content.count('Don\'t be disclosed'))
+
+    def test_person_model(self):
+        "Is model provides correct data"
+        resp = self.client.get(reverse('index'))
+        try:
+            person = Person.objects.get(pk=1)
+        except Person.DoesNotExist:
+            raise AssertionError("Person entity with id 1 does not exisits")
+        self.assertContains(resp, person.name)
+        self.assertContains(resp, person.surname)
+        date = person.date_of_birth.ctime()
+        date = '.'.join(date.split('-')[:-1])
+        print date
+        self.assertContains(resp, date)
+        self.assertContains(resp, person.bio)
+        self.assertContains(resp, person.email)
+        self.assertContains(resp, person.jabber)
+        self.assertContains(resp, person.skype)
+        self.assertContains(resp, person.other_contacts)
+        self.assertContains(resp, person.title)
