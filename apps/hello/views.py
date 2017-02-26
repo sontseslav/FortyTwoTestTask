32856d1fbba016516cde3fea12e5371a1bb0cb8b
sontseslav <s.khvalinsky@gmail.com>
2017-02-26 17:43:27 +0200

    ticket:1 add test for view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -4,18 +4,4 @@ from apps.hello.models import Person
 
 
 class IndexView(TemplateView):
-    template_name = 'hello/index.html'
-    model = Person
-    content = None
-
-    def get(self, request, *args, **kwargs):
-        try:
-            self.content = Person.objects.get(pk=1)
-        except KeyError:
-            raise Http404
-        return super(IndexView, self).get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        content = super(IndexView, self).get_context_data(**kwargs)
-        content['person'] = self.content
-        return content
+    pass
