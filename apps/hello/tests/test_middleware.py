247bb016cd2b314f21e9a0001b65d8722859b06c
sontseslav <s.khvalinsky@gmail.com>
2017-02-27 15:26:43 +0200

    ticket:3 divided test file into parts

--- apps/hello/tests/test_middleware.py
+++ apps/hello/tests/test_middleware.py
@@ -1,36 +1,12 @@
-import re
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from apps.hello.models import MyHttpRequest
 
 
-class RequestDataTests(TestCase):
+class RequestMiddlewareTests(TestCase):
     fixtures = ['initial_data.json']
 
-    def test_request_reachable(self):
-        "Is request page reachable by url name"
-        resp = self.client.get(reverse('requests'))
-        self.assertEqual(resp.status_code, 200)
-
-    def test_model(self):
-        "Is model proprly represents data. Checking by types"
-        request = MyHttpRequest(
-            method="GET",
-            path="/",
-            server_protocol="HTTP/1.1",
-            status=200,
-            response_length=1245
-        )
-        request.save()
-        resp = self.client.get(reverse('requests'))
-        pattern = re.compile(
-            r'<td>(?P<id>\d+)</td>\W+<td>(?P<method>\w{3,5})</td>'
-        )
-        target = pattern.search(resp.content)
-        # target not found?
-        self.assertFalse(target is None)
-
-    def test_middleware_works_not_AJAX(self):
+    def test_middleware_works(self):
         "Is middleware registers requests"
         request = MyHttpRequest.objects.all()
         self.assertEqual(request.count(), 0)
@@ -41,13 +17,3 @@ class RequestDataTests(TestCase):
         request = MyHttpRequest.objects.all()
         # request had been sent 2 times
         self.assertEqual(request.count(), 2)
-
-    def test_view_limit(self):
-        "Is request page shows only 10 last requests"
-        for i in range(10):
-            self.client.get(reverse('index'))
-            self.client.get(reverse('requests'))
-        requests = MyHttpRequest.objects.all()
-        self.assertEqual(requests.count(), 20)
-        response = self.client.get(reverse('requests'))
-        self.assertEqual(response.context['object_list'].count(), 10)
