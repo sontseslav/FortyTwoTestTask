8154cd76cfac34fa459437f7c70a448c666a173d
sontseslav <s.khvalinsky@gmail.com>
2017-03-01 20:21:01 +0200

    fix view tests

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from apps.hello.models import Person
 
@@ -9,10 +9,13 @@ class IndexView(TemplateView):
     content = None
 
     def get(self, request, *args, **kwargs):
-        try:
-            self.content = Person.objects.get(pk=1)
-        except KeyError:
-            raise Http404
+        self.content = Person.objects.first()
+        if not self.content:
+            return render(
+                          request, '404.html',
+                          {'message': 'No etntry exists'},
+                          content_type='application/xhtml+xml'
+                          )
         return super(IndexView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
