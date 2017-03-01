b67e995f3b1ca2f95235a8b0aa000c2b7b7fd355
sontseslav <s.khvalinsky@gmail.com>
2017-03-01 22:39:32 +0200

    ticket:3 add view

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -8,20 +8,6 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Person'
-        db.create_table(u'hello_person', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=25)),
-            ('surname', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
-            ('bio', self.gf('django.db.models.fields.TextField')()),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=50)),
-            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('skype', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('other_contacts', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
-        ))
-        db.send_create_signal(u'hello', ['Person'])
 
         # Adding model 'MyHttpRequest'
         db.create_table(u'hello_myhttprequest', (
@@ -38,8 +24,6 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
-        # Deleting model 'Person'
-        db.delete_table(u'hello_person')
 
         # Deleting model 'MyHttpRequest'
         db.delete_table(u'hello_myhttprequest')
@@ -56,19 +40,6 @@ class Migration(SchemaMigration):
             'server_protocol': ('django.db.models.fields.CharField', [], {'max_length': '12'}),
             'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
             'viewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
-        },
-        u'hello.person': {
-            'Meta': {'object_name': 'Person'},
-            'bio': ('django.db.models.fields.TextField', [], {}),
-            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '50'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
-            'other_contacts': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'surname': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
 
