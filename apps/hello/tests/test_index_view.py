832135fd3d6ca811b48b7e3c16096978df6c50eb
sontseslav <s.khvalinsky@gmail.com>
2017-03-03 23:47:53 +0200

    ticket:1 Add test for view: check context and output

--- apps/hello/tests/test_index_view.py
+++ apps/hello/tests/test_index_view.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from apps.hello.models import Person
@@ -40,15 +41,26 @@ class IndexViewTests(TestCase):
         # context check
         self.assertEquals(resp.context['person'].name, "George")
         self.assertEquals(resp.context['person'].surname, "Petersen")
-        self.assertEquals(resp.context['person'].date_of_birth, "1984-09-28")
+        self.assertEquals(
+            resp.context['person'].date_of_birth, datetime.date(1984, 9, 28)
+            )
         self.assertEquals(resp.context['person'].bio, "Some data")
         self.assertEquals(resp.context['person'].email, "aaa@bbb.ccc")
         self.assertEquals(resp.context['person'].jabber, "jabber@domain.com")
         self.assertEquals(resp.context['person'].skype, "test")
-        self.assertEquals(resp.context['person'].other_contacts, "other contacts")
+        self.assertEquals(
+            resp.context['person'].other_contacts, "other contacts"
+            )
         self.assertEquals(resp.context['person'].title, "Test title # 1")
-        # template puts context values in right places
-
+        # test template
+        self.assertContains(resp, "George")
+        self.assertContains(resp, "Petersen")
+        self.assertContains(resp, "28.09.1984")
+        self.assertContains(resp, "Some data")
+        self.assertContains(resp, "aaa@bbb.ccc")
+        self.assertContains(resp, "jabber@domain.com")
+        self.assertContains(resp, "test")
+        self.assertContains(resp, "other contacts")
         self.assertContains(resp, "Test title # 1")
 
     def test_cyrillic(self):
